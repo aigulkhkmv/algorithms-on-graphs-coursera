b75624c3bb6250900a23d3c18d483bbc405a90d0
aigul <aigul03.14@gmail.com>
2020-06-19 00:34:04 +0300

    NEW: find graph scc

--- week-2/acyclicity.py
+++ week-2/acyclicity.py
@@ -1,55 +1,78 @@
-#Uses python3
+# Uses python3
 
 import sys
 
 
+def reverse(graph):
+    new_chi = [[] for i in graph]
+    for parent_num, old_chis_ in enumerate(graph, 0):
+        for old_chi in old_chis_:
+            new_chi[old_chi].append(parent_num)
+    return new_chi
+
+
 class GraphSearch:
     def __init__(self, graph, visited):
+        self.clock = 0
+        self.post_list = [0 for i in range(len(graph))]
+        self.pre_list = [0 for i in range(len(graph))]
         self.graph = graph
         self.visited = visited
-        self.cycle = False
-        self.reverse_graph = []
-        self.post_count = 0
-        self.post = [0]*len(self.graph)
 
     def explore(self, node):
         self.visited[node] = True
+        self.pre(node)
         for node_child in self.graph[node]:
             if not self.visited[node_child]:
                 self.explore(node_child)
+        self.post(node)
 
     def dfs(self):
         for node in range(len(self.graph)):
             if not self.visited[node]:
                 self.explore(node)
 
-    def reverse(self):
-        new_chi = [[] for i in self.graph]
-        for parent_num, old_chis_ in enumerate(self.graph, 0):
-            for old_chi in old_chis_:
-                new_chi[old_chi].append(parent_num)
-        self.reverse_graph = new_chi
+    def pre(self, node):
+        self.clock += 1
+        self.pre_list[node] = self.clock
 
     def post(self, node):
-        pass
+        self.clock += 1
+        self.post_list[node] = self.clock
+
+
+def find_scc(graph, post_list):
+    for j in range(len(post_list)):
+        max_index = post_list.index(max(post_list))
+        post_list[max_index] = 0
+        if not graph.visited[max_index]:
+            graph.visited[max_index] = True
+            start_clock = graph.clock
+            graph.explore(max_index)
+            if start_clock + 1 < graph.clock:
+                return 1
+            else:
+                return 0
 
 
 def acyclic(adj):
-    visited = [False] * len(adj)
-    graph_search = GraphSearch(adj, visited)
-    graph_search.dfs()
-    if graph_search.cycle:
-        return 1
-    else:
-        return 0
+    visit_list = [False] * len(adj)
+    reverse_graph = reverse(adj)
+
+    reverse_graph_search = GraphSearch(reverse_graph, visit_list)
+    reverse_graph_search.dfs()
+    post_list = reverse_graph_search.post_list
+
+    forvard_graph = GraphSearch(adj, [False] * len(adj))
+    return find_scc(forvard_graph, post_list)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
